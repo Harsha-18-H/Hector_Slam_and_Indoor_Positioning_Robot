63d436e65e80d784bee3c0f5fba81a09386f9fa0
Harsh <harshahrs18@gmail.com>
2025-04-26 13:02:48 +0530

    Initial commit for RoS project

---
+++ build/hector_slam/hector_nav_msgs/CMakeFiles/hector_nav_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/manifest.l"
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/srv/GetDistanceToObstacle.l"
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/srv/GetNormal.l"
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/srv/GetRecoveryInfo.l"
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/srv/GetRobotTrajectory.l"
+  "/home/harsh/catkin_ws/devel/share/roseus/ros/hector_nav_msgs/srv/GetSearchPosition.l"
+  "CMakeFiles/hector_nav_msgs_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hector_nav_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
